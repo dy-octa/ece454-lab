367fe4c66815c79757be17125720d3e6785197aa
dy-octa <dy.octa@outlook.com>
2017-10-04 15:42:05 -0400

    fix bugs and merge ops of same type

--- lab2/implementation.c
+++ lab2/implementation.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <assert.h>
 #include "utilities.h"  // DO NOT REMOVE this line
 #include "implementation_reference.h"   // DO NOT REMOVE this line
 
@@ -10,31 +11,31 @@
  * WARNING: Do not modify the implementation_driver and team info prototype (name, parameter, return value) !!!
  *          Do not forget to modify the team_name and team member information !!!
  **********************************************************************************************************************/
-void print_team_info(){
-    // Please modify this field with something interesting
-    char team_name[] = "Cheesecake";
+void print_team_info() {
+	// Please modify this field with something interesting
+	char team_name[] = "Cheesecake";
 
-    // Please fill in your information
-    char student1_first_name[] = "Andrei";
-    char student1_last_name[] = "Patranoiu";
-    char student1_student_number[] = "998130696";
+	// Please fill in your information
+	char student1_first_name[] = "Andrei";
+	char student1_last_name[] = "Patranoiu";
+	char student1_student_number[] = "998130696";
 
-    // Please fill in your partner's information
-    // If yon't have partner, do not modify this
-    char student2_first_name[] = "Mutian";
-    char student2_last_name[] = "He";
-    char student2_student_number[] = "1004654475";
+	// Please fill in your partner's information
+	// If yon't have partner, do not modify this
+	char student2_first_name[] = "Mutian";
+	char student2_last_name[] = "He";
+	char student2_student_number[] = "1004654475";
 
-    // Printing out team information
-    printf("*******************************************************************************************************\n");
-    printf("Team Information:\n");
-    printf("\tteam_name: %s\n", team_name);
-    printf("\tstudent1_first_name: %s\n", student1_first_name);
-    printf("\tstudent1_last_name: %s\n", student1_last_name);
-    printf("\tstudent1_student_number: %s\n", student1_student_number);
-    printf("\tstudent2_first_name: %s\n", student2_first_name);
-    printf("\tstudent2_last_name: %s\n", student2_last_name);
-    printf("\tstudent2_student_number: %s\n", student2_student_number);
+	// Printing out team information
+	printf("*******************************************************************************************************\n");
+	printf("Team Information:\n");
+	printf("\tteam_name: %s\n", team_name);
+	printf("\tstudent1_first_name: %s\n", student1_first_name);
+	printf("\tstudent1_last_name: %s\n", student1_last_name);
+	printf("\tstudent1_student_number: %s\n", student1_student_number);
+	printf("\tstudent2_first_name: %s\n", student2_first_name);
+	printf("\tstudent2_last_name: %s\n", student2_last_name);
+	printf("\tstudent2_student_number: %s\n", student2_student_number);
 }
 
 /***********************************************************************************************************************
@@ -50,26 +51,49 @@ void print_team_info(){
  ***********************************************************************************************************************
  *
  **********************************************************************************************************************/
- typedef struct {
-	 char typeA;
-	 int B[2];
- } Matrix;
+typedef struct {
+	char typeA;
+	int B[2];
+} Matrix;
 char typeAMuls[] = {0, 1, 2, 3, 4, 5, 6, 7, 1, 0, 3, 2, 6, 7, 4, 5, 2, 3, 0, 1, 5, 4, 7, 6, 3, 2, 1, 0, 7, 6, 5, 4, 4,
                     5, 6, 7, 0, 1, 2, 3, 5, 4, 7, 6, 2, 3, 0, 1, 6, 7, 4, 5, 1, 0, 3, 2, 7, 6, 5, 4, 3, 2, 1, 0};
-void multiMatrix(Matrix* A, Matrix* B, Matrix* C){
-	C->typeA = typeAMuls[((short)(A->typeA))<<3 | ((short)(B->typeA))];
-	if ((B->typeA >> 2) & 1) {
-		C->B[0] = A->B[1];
-		C->B[1] = A->B[0];
+
+void multiMatrix(Matrix *A, Matrix *B, Matrix *C) {
+	C->typeA = typeAMuls[((short) (A->typeA)) << 3 | ((short) (B->typeA))];
+	switch (B->typeA) {
+		case 0:
+			C->B[0] = A->B[0];
+			C->B[1] = A->B[1];
+			break;
+		case 1:
+			C->B[0] = A->B[0];
+			C->B[1] = -A->B[1];
+			break;
+		case 2:
+			C->B[0] = -A->B[0];
+			C->B[1] = A->B[1];
+			break;
+		case 3:
+			C->B[0] = -A->B[0];
+			C->B[1] = -A->B[1];
+			break;
+		case 4:
+			C->B[0] = A->B[1];
+			C->B[1] = A->B[0];
+			break;
+		case 5:
+			C->B[0] = A->B[1];
+			C->B[1] = -A->B[0];
+			break;
+		case 6:
+			C->B[0] = -A->B[1];
+			C->B[1] = A->B[0];
+			break;
+		case 7:
+			C->B[0] = -A->B[1];
+			C->B[1] = -A->B[0];
+			break;
 	}
-	else {
-		C->B[0] = A->B[0];
-		C->B[1] = A->B[1];
-	}
-	if (B->typeA & 1)
-		C->B[1] = -C->B[1];
-	if ((B->typeA >> 1) & 1)
-		C->B[0] = -C->B[0];
 	C->B[0] += B->B[0];
 	C->B[1] += B->B[1];
 	return;
@@ -94,13 +118,16 @@ void multiMatrix(Matrix* A, Matrix* B, Matrix* C){
 //{0 -1; -1 0}
 void implementation_driver(struct kv *sensor_values, int sensor_values_count, unsigned char *frame_buffer,
                            unsigned int width, unsigned int height, bool grading_mode) {
-    int processed_frames = 0;
-	Matrix final_matrix[2] = {{0, 0, 0}, {0, 0, 0}}; // {{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}, {}};
+	int processed_frames = 0;
+	Matrix final_matrix[2] = {{0, 0, 0},
+	                          {0, 0, 0}}; // {{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}}, {}};
 	Matrix matUp = {0, 0, 0}; // {{1, 0, 0}, {0, 1, 0}, {-1, 0, 1}};
 	Matrix matDown = {0, 0, 0}; // {{1, 0, 0}, {0, 1, 0}, {1, 0, 1}};
 	Matrix matLeft = {0, 0, 0}; // {{1, 0, 0}, {0, 1, 0}, {0, -1, 1}};
 	Matrix matRight = {0, 0, 0}; // {{1, 0, 0}, {0, 1, 0}, {0, 1, 1}};
-	Matrix matRot[3] = {{5, 0, width - 1}, {3, width - 1, width - 1}, {6, width - 1, 0}};
+	Matrix matRot[3] = {{5, 0,         width - 1},
+	                    {3, width - 1, width - 1},
+	                    {6, width - 1, 0}};
 //						{{{0, -1, 0}, {1, 0, 0}, {0, width - 1, 1}},
 //						 {{-1, 0, 0}, {0, -1, 0}, {width - 1, width - 1, 1}},
 //						 {{0, 1, 0}, {-1, 0, 0}, {width - 1, 0, 1}}};
@@ -109,147 +136,178 @@ void implementation_driver(struct kv *sensor_values, int sensor_values_count, un
 	int current_matrix = 0;
 
 	//int testArray[10669];
-	int * testArray = malloc (300000 * sizeof(int));
+	int *testArray = malloc(300000 * sizeof(int));
 	//int testRow[10669];
-	int * testRow = malloc (300000 * sizeof(int));
+	int *testRow = malloc(300000 * sizeof(int));
 	//int testCol[10669];
-	int * testCol = malloc (300000 * sizeof(int));
-	int nPix = 0;
+	int *testCol = malloc(300000 * sizeof(int));
+	int i = 0;
 	int pos = 0;
-	int resizeFlag = 300000;
+	int resizeFlag = 299000;
 
-	/*unsigned char * buffer = (unsigned char *) malloc(sizeof(16 * sizeof(unsigned char))); //{ 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff };
-	buffer[0] = 255;
-	buffer[1] = 255;
-	buffer[2] = 255;
-	buffer[3] = 255;
-	buffer[4] = 255;
-	buffer[5] = 255;
-	buffer[6] = 255;
-	buffer[7] = 255;
-	buffer[8] = 255;
-	buffer[9] = 0;
-	buffer[10] = 0;
-	buffer[11] = 0;
-	buffer[12] = 0x0;
-	buffer[13] = 0x0;
-	buffer[14] = 0x0;
-	buffer[15] = 0x0;
-*/
 
-	//unsigned long long * tester = (unsigned long long *) frame_buffer;
-
-			//printf("TESTING TESTER1: %llu\n", *tester);
-			//printf("TESTING TESTER: %llu\n", *tester & 0xFFFFFF);
-			//printf("TESTING BUFFER: %d\n", (width * height)/8);
-
-    //printf("what is width*height: %d, %d, %d\n", width*height, width*height*3, (width*height*3)/8);
-
-	/*for (int f = 0; f < ((width*height*3)/8); f++){	//(/6 has equal iterations to normal loop)
-		//printf("TESTING TESTER: %llu\n", tester[0]);
-		//printf("TESTING TESTER2: %llu\n", tester[1] & 0xff);
-		if(tester[f] != 18446744073709551615){
-			printf("TESTING TESTER: %llu\n", tester[f]);
-			printf("TESTING TESTER2: %llu\n", tester[f+1] & 0xff);
-		}
-	}*/
-
-    //char filename[100];
-    //sprintf(filename, "%dimp.bmp", processed_frames);
-    //writeBMP(width, height, render_buffer, filename);
-
-	//unsigned long * tester = (unsigned long *) &frame_buffer[0];
-
-	for (int row = 0; row < height; ++row){
+	for (int row = 0; row < height; ++row) {
 		pos = row * width * 3;
 		for (int col = 0; col < width; ++col) {
-			 int posTest = pos + col * 3;
-			if(!((((unsigned int *) &frame_buffer[posTest])[0] & 0xFFFFFF) == 16777215)){
-
-			 testArray[nPix] = posTest;
-			 testRow[nPix] = row;
-			 testCol[nPix] = col;
-			 nPix++;
-				if(nPix == resizeFlag ){
+			int posTest = pos + col * 3;
+			if ((((unsigned int *) &frame_buffer[posTest])[0] & 0xFFFFFF) != 16777215) {
+				//if(frame_buffer[posTest] != 255 && frame_buffer[posTest + 1] != 255 && frame_buffer[posTest+2] != 255){
+				//if (!(frame_buffer[posTest] == 255 && frame_buffer[posTest + 1] == 255 && frame_buffer[posTest + 2] == 255)) {
+				testArray[i] = posTest;
+				testRow[i] = row;
+				testCol[i] = col;
+				i++;
+				if (i == resizeFlag) {
 					resizeFlag = resizeFlag * 2;
-					testArray = realloc(testArray, resizeFlag * sizeof (int));
-					testRow = realloc(testRow, resizeFlag * sizeof (int));
-					testCol = realloc(testCol, resizeFlag * sizeof (int));
+					testArray = realloc(testArray, resizeFlag * sizeof(int));
+					testRow = realloc(testRow, resizeFlag * sizeof(int));
+					testCol = realloc(testCol, resizeFlag * sizeof(int));
 				}
 			}
 		}
 	}
 
-	unsigned char* render_buffer = allocateFrame(width, height);
+	unsigned char *render_buffer = allocateFrame(width, height);
 
-    for (int sensorValueIdx = 0; sensorValueIdx < sensor_values_count; sensorValueIdx++) {
-        if (!strcmp(sensor_values[sensorValueIdx].key, "W")) {
-	        matUp.B[0] = - sensor_values[sensorValueIdx].value;
-	        multiMatrix(&final_matrix[current_matrix], &matUp, &final_matrix[current_matrix^1]);
-        } else if (!strcmp(sensor_values[sensorValueIdx].key, "A")) {
-	        matLeft.B[1] = - sensor_values[sensorValueIdx].value;
-	        multiMatrix(&final_matrix[current_matrix], &matLeft, &final_matrix[current_matrix^1]);
-        } else if (!strcmp(sensor_values[sensorValueIdx].key, "S")) {
-	        matDown.B[0] = sensor_values[sensorValueIdx].value;
-	        multiMatrix(&final_matrix[current_matrix], &matDown, &final_matrix[current_matrix^1]);
-        } else if (!strcmp(sensor_values[sensorValueIdx].key, "D")) {
-	        matRight.B[1] = sensor_values[sensorValueIdx].value;
-	        multiMatrix(&final_matrix[current_matrix], &matRight, &final_matrix[current_matrix^1]);
-        } else if (!strcmp(sensor_values[sensorValueIdx].key, "CW") || !strcmp(sensor_values[sensorValueIdx].key, "CCW")) {
-	        int itr = sensor_values[sensorValueIdx].value;
-	        if (!strcmp(sensor_values[sensorValueIdx].key, "CCW"))
-		        itr = - itr;
-	        itr %= 4;
-	        if (itr < 0)
-		        itr = itr + 4;
-	        if (itr != 0)
-	            multiMatrix(&final_matrix[current_matrix], &matRot[itr - 1], &final_matrix[current_matrix^1]);
-	        else current_matrix ^= 1;
-        } else if (!strcmp(sensor_values[sensorValueIdx].key, "MX")) {
-	        multiMatrix(&final_matrix[current_matrix], &matFlipX, &final_matrix[current_matrix^1]);
-        } else if (!strcmp(sensor_values[sensorValueIdx].key, "MY")) {
-	        multiMatrix(&final_matrix[current_matrix], &matFlipY, &final_matrix[current_matrix^1]);
-        }
-	    current_matrix ^= 1;
-        processed_frames += 1;
+	for (int sensorValueIdx = 0; sensorValueIdx < sensor_values_count; sensorValueIdx++) {
+
+		int multi_instr = 0;
+
+		if (!strcmp(sensor_values[sensorValueIdx].key, "W")) {
+			multi_instr = sensor_values[sensorValueIdx].value;
+
+
+			while (sensorValueIdx + 1 < sensor_values_count && !strcmp(sensor_values[sensorValueIdx + 1].key, "W") &&
+			       (processed_frames + 1) % 25 != 0) {
+
+				multi_instr += sensor_values[sensorValueIdx + 1].value;
+				sensorValueIdx++;
+				processed_frames++;
+			}
+
+			//matUp.B[0] = - sensor_values[sensorValueIdx].value;
+			matUp.B[0] = -multi_instr;
+			multiMatrix(&final_matrix[current_matrix], &matUp, &final_matrix[current_matrix ^ 1]);
+		} else if (!strcmp(sensor_values[sensorValueIdx].key, "A")) {
+			multi_instr = sensor_values[sensorValueIdx].value;
+			while (sensorValueIdx + 1 < sensor_values_count && !strcmp(sensor_values[sensorValueIdx + 1].key, "A") &&
+			       (processed_frames + 1) % 25 != 0) {
+
+				multi_instr += sensor_values[sensorValueIdx + 1].value;
+				sensorValueIdx++;
+				processed_frames++;
+			}
+
+			//matLeft.B[1] = - sensor_values[sensorValueIdx].value;
+			matLeft.B[1] = -multi_instr;
+			multiMatrix(&final_matrix[current_matrix], &matLeft, &final_matrix[current_matrix ^ 1]);
+		} else if (!strcmp(sensor_values[sensorValueIdx].key, "S")) {
+			multi_instr = sensor_values[sensorValueIdx].value;
+			while (sensorValueIdx + 1 < sensor_values_count && !strcmp(sensor_values[sensorValueIdx + 1].key, "S") &&
+			       (processed_frames + 1) % 25 != 0) {
+
+				multi_instr += sensor_values[sensorValueIdx + 1].value;
+				sensorValueIdx++;
+				processed_frames++;
+			}
+
+			//matDown.B[0] = sensor_values[sensorValueIdx].value;
+			matDown.B[0] = multi_instr;
+			multiMatrix(&final_matrix[current_matrix], &matDown, &final_matrix[current_matrix ^ 1]);
+		} else if (!strcmp(sensor_values[sensorValueIdx].key, "D")) {
+			multi_instr = sensor_values[sensorValueIdx].value;
+			while (sensorValueIdx + 1 < sensor_values_count && !strcmp(sensor_values[sensorValueIdx + 1].key, "D") &&
+			       (processed_frames + 1) % 25 != 0) {
+
+				multi_instr += sensor_values[sensorValueIdx + 1].value;
+				sensorValueIdx++;
+				processed_frames++;
+			}
+
+			//matRight.B[1] = sensor_values[sensorValueIdx].value;
+			matRight.B[1] = multi_instr;
+			multiMatrix(&final_matrix[current_matrix], &matRight, &final_matrix[current_matrix ^ 1]);
+		} else if (!strcmp(sensor_values[sensorValueIdx].key, "CW") ||
+		           !strcmp(sensor_values[sensorValueIdx].key, "CCW")) {
+			int itr = sensor_values[sensorValueIdx].value;
+			if (!strcmp(sensor_values[sensorValueIdx].key, "CCW"))
+				itr = -itr;
+			itr %= 4;
+			if (itr < 0)
+				itr = itr + 4;
+			if (itr != 0)
+				multiMatrix(&final_matrix[current_matrix], &matRot[itr - 1], &final_matrix[current_matrix ^ 1]);
+			else current_matrix ^= 1;
+		} else if (!strcmp(sensor_values[sensorValueIdx].key, "MX")) {
+			multiMatrix(&final_matrix[current_matrix], &matFlipX, &final_matrix[current_matrix ^ 1]);
+		} else if (!strcmp(sensor_values[sensorValueIdx].key, "MY")) {
+			multiMatrix(&final_matrix[current_matrix], &matFlipY, &final_matrix[current_matrix ^ 1]);
+		}
+		current_matrix ^= 1;
+		processed_frames += 1;
 //	    printf("%d\n", processed_frames);
-        if (processed_frames % 25 == 0) {
+		if (processed_frames % 25 == 0) {
 //	        printf("Process %d...\n", processed_frames);
-	        memset(render_buffer, 255, width * height * 3);
-	        for (int j = 0; j < nPix; j++){
-		        int render_row, render_col, typeA = final_matrix[current_matrix].typeA;
-		        if ((typeA >> 2) & 1) {
-			        render_row = testCol[j];
-			        render_col = testRow[j];
-		        }
-		        else {
-			        render_row = testRow[j];
-			        render_col = testCol[j];
-		        }
-		        if (typeA & 1)
-			        render_col = -render_col;
-		        if ((typeA >> 1) & 1)
-			        render_row = -render_row;
-		        render_row += final_matrix[current_matrix].B[0];
-		        render_col += final_matrix[current_matrix].B[1];
-//				if (render_row < 0 || render_row >= height || render_col < 0 || render_col >=width)
-//		            printf("!!!(%d, %d)->(%d, %d)!!!\n", testRow[j], testCol[j], render_row, render_col);
-	        	int render_pos = render_row * width * 3 + render_col * 3;
-	        	render_buffer[render_pos] = frame_buffer[testArray[j]];
-	        	render_buffer[render_pos + 1] = frame_buffer[testArray[j] + 1];
-	        	render_buffer[render_pos + 2] = frame_buffer[testArray[j] + 2];
+			memset(render_buffer, 255, width * height * 3);
 
-	        }
-            verifyFrame(render_buffer, width, height, grading_mode);
+			for (int j = 0; j < i; j++) {
+				int render_row = final_matrix[current_matrix].B[0];
+				int render_col = final_matrix[current_matrix].B[1];
+				switch (final_matrix[current_matrix].typeA) {
+					case 0:
+						render_row += testRow[j];
+						render_col += testCol[j];
+						break;
+					case 1:
+						render_row += testRow[j];
+						render_col -= testCol[j];
+						break;
+					case 2:
+						render_row -= testRow[j];
+						render_col += testCol[j];
+						break;
+					case 3:
+						render_row -= testRow[j];
+						render_col -= testCol[j];
+						break;
+					case 4:
+						render_row += testCol[j];
+						render_col += testRow[j];
+						break;
+					case 5:
+						render_row += testCol[j];
+						render_col -= testRow[j];
+						break;
+					case 6:
+						render_row -= testCol[j];
+						render_col += testRow[j];
+						break;
+					case 7:
+						render_row -= testCol[j];
+						render_col -= testRow[j];
+						break;
+				}
 
-//	        char filename[100];
-//	        sprintf(filename, "%dimp.bmp", processed_frames);
-//	        writeBMP(width, height, render_buffer, filename);
-        }
-    }
-	free (testArray);
-	free (testRow);
-	free (testCol);
+
+				int render_pos = render_row * width * 3 + render_col * 3;
+				//assert(render_row => 0 && render_row <= height);//testing failed
+				//assert(render_col => 0 && render_col <= width);//testing failed
+				//assert(render_pos => 0 && render_pos <= width*height*3);//testing failed
+				//printf("render pos is: %d\n", render_pos);
+				//if(render_pos > 0 && render_pos < height*width*3){
+				render_buffer[render_pos] = frame_buffer[testArray[j]];
+				render_buffer[render_pos + 1] = frame_buffer[testArray[j] + 1];
+				render_buffer[render_pos + 2] = frame_buffer[testArray[j] + 2];
+				//}
+
+			}
+			verifyFrame(render_buffer, width, height, grading_mode);
+
+		}
+	}
+	free(testArray);
+	free(testRow);
+	free(testCol);
 	deallocateFrame(render_buffer);
-    return;
+	return;
 }
